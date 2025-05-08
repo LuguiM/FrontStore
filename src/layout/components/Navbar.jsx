a65232958d31ab66582aa7e4e0990e5dc3e6d414
LuigiM <jlpadilla@innovacion.gob.sv>
2025-05-08 10:05:05 -0600

    Feat: Estructura base del navbar

--- src/layout/components/Navbar.jsx
+++ src/layout/components/Navbar.jsx
@@ -1,19 +1,48 @@
+import { NavLink } from "react-router"
+
+const routes = [
+  {
+    href: "/",
+    label: "Home",
+  },
+  {
+    href: "/products",
+    label: "Products"
+  },
+  {
+    href: "/categories",
+    label: "Categories"
+  }
+]
 
 export const Navbar = () => {
   return (
     <div className="border-b border-gray container-spacing">
-        <div className=" flex justify-between items-center h-16">
-            <div className="flex items-center gap-2">
-            <img src="../../../public/vite.svg" alt="Logo" className="w-10 h-10" />
-            <h1 className="text-2xl font-bold">FrontStore</h1>
-            </div>
-            <nav className="flex gap-4">
-            <a href="/" className="text-gray-700 hover:text-gray-900">Home</a>
-            <a href="/products" className="text-gray-700 hover:text-gray-900">Products</a>
-            <a href="/about" className="text-gray-700 hover:text-gray-900">About</a>
-            <a href="/contact" className="text-gray-700 hover:text-gray-900">Contact</a>
-            </nav>
+      <div className=" flex justify-between items-center h-16">
+        <div className="flex items-center gap-2">
+          <img src="/vite.svg" alt="Logo" className="w-10 h-10" />
+          <h1 className="text-2xl font-bold">FrontStore</h1>
         </div>
+        <nav className="flex gap-x-9 text-bodyRegular">
+          {
+            routes.map((route) => (
+              <NavLink
+                key={route.href}
+                to={route.href}
+                style={({ isActive, isTransitioning }) => {
+                  return {
+                    fontWeight: isActive ? "bold" : "",
+                    viewTransitionName: isTransitioning ? "slide" : "",
+                  };
+                }}
+              >
+                {route.label}
+              </NavLink>
+            ))
+          }
+
+        </nav>
+      </div>
     </div>
   )
 }
