da73ac313877346450d350067657d14e075e7ca5
LuigiM <jlpadilla@innovacion.gob.sv>
2025-05-09 10:31:06 -0600

    Fix(HeroSection): Posicion de los circulos

--- src/views/home/Hero.jsx
+++ src/views/home/Hero.jsx
@@ -1,17 +1,17 @@
 import { Button, Circle } from "../../components"
 
 const categories = [
-    { name: "Jewellery", position: "end" },
-    { name: "WomanClothes", position: "start" },
-    { name: "MenClothes", position: "start" },
-    { name: "Accesories", position: "end" }
+    { name: "Jewellery", position: "items-end justify-end" },
+    { name: "WomanClothes", position: "items-start justify-start" },
+    { name: "MenClothes", position: "items-start justify-start" },
+    { name: "Accesories", position: "items-end justify-start" }
 ]
 
 export const Hero = () => {
     return (
         <section className='flex flex-col items-center gap-y-6'>
             <div className='text-center mx-15 xl:mx-25'>
-                <p className='text-h6 !font-bold'>Meet the Store</p>
+                <p className='text-h6 !font-bold text-ligth-pink'>Meet the Store</p>
                 <h1>
                     <span className='text-h5 md:text-h2 tracking-tight'>Know your true style with</span>
                     <br className="sm:hidden" />
@@ -20,10 +20,10 @@ export const Hero = () => {
 
             </div>
                 <Button label="Shop now" isLink />
-            <div class="hidden lg:grid grid-cols-4 gap-x-16 h-64">
+            <div className="hidden lg:grid grid-cols-4 gap-x-16 h-64">
                 {
                     categories.map(c => (
-                        <div class={`px-4 flex items-${c.position} justify-${c.position}`}>
+                        <div key={c.name} className={`px-4 flex ${c.position}`}>
                             <Circle categorie={c.name} />
                         </div>
                     ))
