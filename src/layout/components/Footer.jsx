385e055eb837a6e9e7fdd2ce5d8c2ff9c7b8f2b4
LuigiM <jlpadilla@innovacion.gob.sv>
2025-05-12 15:17:41 -0600

    Feat: Footer

--- src/layout/components/Footer.jsx
+++ src/layout/components/Footer.jsx
@@ -2,7 +2,14 @@
 
 export const Footer = () => {
   return (
-    <div className="border-t border-gray container-spacing">
-      Footer</div>
+    <div className="border-t border-gray container-spacing flex justify-between items-center !py-10">
+      <div className="flex items-center gap-2">
+        <img src="/vite.svg" alt="Logo" className="w-10 h-10" />
+        <h1 className="text-2xl font-bold">FrontStore</h1>
+      </div>
+      <div className="flex flex-col gap-2">
+        <p className="text-bodyRegular">© 2024 FrontStore. All rights reserved.</p>
+      </div>
+    </div>
   )
 }
