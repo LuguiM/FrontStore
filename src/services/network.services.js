aae852a4b53f6335cd2521ba3ef89aa752853269
LuigiM <jlpadilla@innovacion.gob.sv>
2025-05-13 14:46:27 -0600

    Feat: Configuracion de services

---
+++ src/services/network.services.js
@@ -0,0 +1,35 @@
+export default async function Config(method, url, body = {}) {
+    const app_url = 'https://fakestoreapi.com'
+
+    const config = () => {
+        if (method === 'GET') {
+            return {
+                method: method
+            }
+        }
+        return {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body)
+        }
+    }
+
+    try{
+
+        const api = await fetch(app_url + url, config())
+        if (!api.ok) { 
+            throw new Error(`HTTP error! Status: ${api.status}`);
+        }
+        const res = await api.json();
+        return res;
+
+    }catch(error){
+
+        console.log(error);
+        return {}
+
+    }
+   
+}
