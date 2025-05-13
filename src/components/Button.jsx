3d6a94d86393a01c819f3fe68f2f6e10bae3ecd4
LuigiM <jlpadilla@innovacion.gob.sv>
2025-05-13 10:36:19 -0600

    Feat: CardCategory componente

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,12 @@
 import { NavLink } from "react-router"
 
 export const Button = ({ isLink, outlined, label, className }) => {
+    const isLinkString = typeof isLink === "string";
+    const link = isLinkString ? isLink : "/products";
+
     return (
         isLink ? (
-            <NavLink to="/products" className={`${className} ${outlined ? "button-outlined" : "button-base"}`}>
+            <NavLink to={link} className={`${className} ${outlined ? "button-outlined" : "button-base"}`}>
                 {label}
             </NavLink>
         ) : (
