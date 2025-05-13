1969161b35519f9b7d6fcbdad667bfa3af26f3a0
LuigiM <jlpadilla@innovacion.gob.sv>
2025-05-13 15:58:56 -0600

    Feat: Input y Select componentes

---
+++ src/components/Select.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export const Select = ({ options = [], value, onChange, placeholder = "Categories" }) => {
+    const [open, setOpen] = useState(false);
+
+    const handleSelect = (option) => {
+        onChange(option);
+        setOpen(false);
+    };
+
+    return (
+        <div className="relative w-54">
+            <button
+                onClick={() => setOpen(!open)}
+                className="w-full px-5 py-3  border rounded-md shadow text-gray-400 border-ligth-blue flex gap-5 items-center justify-between"
+            >
+                <span className={value ? "text-gray-700" : "text-gray-400"}>
+                    {value || placeholder}
+                </span>
+                <div>d</div>
+            </button>
+
+            {open && (
+                <ul className="absolute z-10 w-full mt-1 bg-white border rounded-md shadow-lg">
+                    {options.map((option) => (
+                        <li
+                            key={option}
+                            onClick={() => handleSelect(option)}
+                            className="px-4 py-2 hover:bg-blue-100 cursor-pointer"
+                        >
+                            {option}
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+};
